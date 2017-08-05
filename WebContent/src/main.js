0a6cdfb6e0c8ec6d13d7b15def430e96cbd14299
Infko <r@g.gg>
2017-08-05 10:58:44 +0800

    修正了lint问题

--- WebContent/src/main.js
+++ WebContent/src/main.js
@@ -1,12 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import ElementUI from 'element-ui'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+import ElementUI from 'element-ui';
 
-Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 /* eslint-disable no-new */
 new Vue({
@@ -14,4 +14,4 @@ new Vue({
   router,
   template: '<App/>',
   components: { App }
-})
+});
